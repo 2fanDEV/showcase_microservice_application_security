ac8ddaaaee7e41af928afe979524f8f2adce5055
2fanDEV <tufan.oezen@protonmail.com>
2023-04-18 20:52:32 +0200

    vaultservice created

---
+++ VaultService/src/main/java/de/security/microservicec/vaultservice/VaultServiceApplication.java
@@ -0,0 +1,13 @@
+package de.security.microservicec.vaultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultServiceApplication.class, args);
+	}
+
+}
---
+++ VaultService/src/test/java/de/security/microservicec/vaultservice/VaultServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.security.microservicec.vaultservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
